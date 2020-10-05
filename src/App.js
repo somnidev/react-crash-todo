2331f17a7196346933a5b556b07e86f51abb4900
Simon Oser <simon.oser@gmail.com>
2020-10-05 15:32:33 +0200

    Moved to classes instead functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Todos />
+            </div>
+        );
+    }
 }
 
 export default App;
